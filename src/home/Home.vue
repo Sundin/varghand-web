c16bb21252dec6a53f58220c3f46a860ff4d88ad
Gustav Sundin <sundin666@gmail.com>
2021-12-06 09:53:06 +0100

    Link to Mace & Magic

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -2,6 +2,8 @@
   <main-layout>
     <div class=content>
       <img alt="Varghand logo" class="logo" src="../assets/varghand.png">
+      <h1>Our games:</h1>
+      <a href="https://www.sound-realms.com"><img src="../assets/dreadhorn.png" class="logo" /></a>
     </div>
   </main-layout>
 </template>
@@ -9,9 +11,13 @@
 <style scoped>
 .content {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  text-align: center;
 }
+
+
 .logo {
   padding: 12px;
   max-width: 90%;
